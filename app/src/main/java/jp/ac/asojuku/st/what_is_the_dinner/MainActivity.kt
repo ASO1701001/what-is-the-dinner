983bf26842010f6298a76aab38454fe927072d18
ASO1701002 <1701002@st.asojuku.ac.jp>
2019-05-10 23:57:51 +0900

    ログイン機能を追加　画面遷移とユーザーの分岐は未実装

--- app/src/main/java/jp/ac/asojuku/st/what_is_the_dinner/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/st/what_is_the_dinner/MainActivity.kt
@@ -1,12 +1,29 @@
 package jp.ac.asojuku.st.what_is_the_dinner
 
-import android.support.v7.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val am:AccountManager = AccountManager(this)
+        rogin_registration_button.setOnClickListener { am.registaration(); }
+        rogin_rogin_button.setOnClickListener { am.rogin() }
+
     }
 }
+
